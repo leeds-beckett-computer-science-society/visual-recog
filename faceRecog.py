4cad652ed0bd02f06c293058f7366daabbb6f71f
Sam Lightowler <slightowler7@gmail.com>
2019-12-11 01:13:34 +0000

    cleaned code up

--- faceRecog.py
+++ faceRecog.py
@@ -1,50 +1,32 @@
 import cv2
-import time
 
 
-# utilizes haarcascade
-face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+#
 
-# enter in webcam path or 0,1,2,3
-video = cv2.VideoCapture(0)
+def visualrec():
+    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    video = cv2.VideoCapture(0)
 
-# use this variable for counting frames
-a = 1
+    while True:
+        check, frame = video.read()
+        gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray_img,
+                                              scaleFactor=1.05,
+                                              minNeighbors=5)
 
-# loops through each video frame
-while True:
-    a = a+1
+        # take position values from face_cascade and draw rectangle around face
+        for x, y, w, h in faces:
+            gray_img = cv2.rectangle(gray_img, (x, y), (x + w, y + h), (255, 255, 255), 1)
+            cv2.putText(gray_img, 'Face', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 1)
 
-    # initialise video frames and print values
-    check, frame = video.read()
-    print(check)
-    print(frame)
+        # opens window to view webcam
+        cv2.imshow("idiot on screen", gray_img)
+        key = cv2.waitKey(1)
 
-    # convert frame to grayscale (higher accuracy)
-    gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # set parameter for how the algorithm approaches the frame
-    # 1.05 means each iteration of the frame the image is decreased 5%
-    faces = face_cascade.detectMultiScale(gray_img,
-     scaleFactor=1.05,
-     minNeighbors=5)
-
-    # take position values from face_cascade and draw rectangle around face
-    for x, y, w, h in faces:
-        gray_img = cv2.rectangle(gray_img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        # exit key
+        if key == ord('q'):
+            break
+    cv2.destroyAllWindows()
 
 
-    # opens window to view webcam
-    cv2.imshow("Capture", gray_img)
-
-    # each iteration of loop waits 1 millisecond
-    # can increase to have lesser framerate for optimization
-    key = cv2.waitKey(1)
-
-    # exit key
-    if key == ord ('q'):
-        break
-
-print(a)
-video.release()
-cv2.destroyAllWindows()
+visualrec()
